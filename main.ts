71fbc526a9e3f3582ec34b99e2405b03df4d42d3
adi-tya-shah <shaha26@stxtigers.com>
2024-11-15 13:09:45 -0500

    Restore '11/14/2024, 1:56:23 PM Update main.blocks, main.ts'

--- main.ts
+++ main.ts
@@ -1,5 +1,4 @@
-let mySprite: Sprite = null
-function doSomething (mySprite: Sprite) {
+function mysprite (mySprite: Sprite) {
     mySprite = sprites.create(img`
         . . . . . . . . . . . . . . . . 
         . . . . . f f f f f f . . . . . 
@@ -8,14 +7,41 @@ function doSomething (mySprite: Sprite) {
         . . f 7 7 7 f f 7 7 7 7 f . . . 
         . . f f f f 7 7 7 7 7 7 7 f . . 
         . . f 7 7 7 7 f f f f 7 7 f . . 
-        . f f f f f f f e e e f f f . . 
-        . f f e 4 4 e b f 4 4 e e f . . 
-        . f e e 4 d 4 1 f d d e f . . . 
-        . . f e e e e e d d d f . . . . 
-        . . . . f 4 d d e 4 e f . . . . 
-        . . . . f e d d e 2 2 f . . . . 
-        . . . f f f e e f 5 5 f f . . . 
+        . f f f f f f f 6 6 6 f f f . . 
+        . f f 6 4 4 6 7 f 4 4 6 6 f . . 
+        . f 6 6 4 d 4 1 f 7 7 6 f . . . 
+        . . f 6 6 6 6 6 7 7 b b b b b b 
+        . . . . f 4 7 7 6 b b f . . . . 
+        . . . . f 6 7 7 6 c c f . . . . 
+        . . . f f f 6 6 f 8 8 f f . . . 
         . . . f f f f f f f f f f . . . 
         . . . . f f . . . f f f . . . . 
         `, SpriteKind.Player)
+    mySprite.setPosition(4, 75)
+    controller.moveSprite(mySprite)
+    mySprite.setStayInScreen(true)
 }
+let tank: Sprite = null
+let mySprite: Sprite = null
+mysprite(mySprite)
+game.onUpdateInterval(500, function () {
+    tank = sprites.create(img`
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . 7 7 7 7 7 7 7 7 . . . . . . 
+        . . . . . . 7 7 7 7 . . . . . . 
+        . . . . . 7 7 7 7 7 7 7 7 . . . 
+        . . . 7 7 7 7 7 7 7 7 7 7 . . . 
+        . . . 7 7 7 7 7 7 7 7 7 7 . . . 
+        . . . . f f f f f f f . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        `, SpriteKind.Enemy)
+    tank.setVelocity(randint(0, 70), randint(0, 70))
+})
