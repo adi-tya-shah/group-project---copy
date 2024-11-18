45909eb7a7781f45d6232f11bfc21edd7b56419a
adi-tya-shah <shaha26@stxtigers.com>
2024-11-18 13:23:05 -0500

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -8,16 +8,16 @@ controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
         . . . . . . . . . . . . . . . . 
         . . . . . . . . . . . . . . . . 
         . . . . . . . . . . . . . . . . 
-        . . . . . . . . . . f f . . . . 
-        . . . . . . . . . . f f f f . . 
-        . . . f f f f f f f f 6 f f f . 
-        . . . f 7 7 7 7 7 7 f 6 6 6 f f 
-        . . . f 7 7 7 7 7 7 f 6 6 6 f f 
-        . . . f f f f f f f f 6 f f f . 
-        . . . . . . . . . . f f f f . . 
-        . . . . . . . . . . f f . . . . 
         . . . . . . . . . . . . . . . . 
         . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . f f . . . . . 
+        . . . . . . . . . f f f f . . . 
+        . . . f f f f f f f 6 f f f . . 
+        . . . f 7 7 7 7 7 f 6 6 6 f . . 
+        . . . f f f f f f f 6 f f f . . 
+        . . . . . . . . . f f f f . . . 
+        . . . . . . . . . f f . . . . . 
         . . . . . . . . . . . . . . . . 
         `, mySprite, 75, 0)
 })
